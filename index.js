4b380933e3d642fa613e7193e51532a9ce831872
Connor Meth <connormeth27@gmail.com>
2019-10-27 17:11:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const driver = 'bob';
+const var2 = ', is the driver variable name';
+
+
+    console.log("this code was called");
+    console.log(`${driver}`);
+    console.log(`${driver}`, 'is the driver variable name');
+
